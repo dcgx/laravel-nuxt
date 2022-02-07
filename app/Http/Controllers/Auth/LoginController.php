c7b0d2987a2511dc4a4dc2e1abe91895f9e2a9bf
Diego Castillo <diego.castilloguajardo@gmail.com>
2022-02-07 14:04:00 -0300

    config: Config auth.php with api guard, add LoginController

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Exceptions\VerifyEmailException;
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $user = $request->user();
+        $user->tokens()->delete();
+    }
+
+    protected function attemptLogin(Request $request) 
+    {
+        $isAttemp = $this->guard()->attempt($this->credentials($request));
+
+        if (! $isAttemp) {
+            return false;
+        }
+
+        $user = $this->guard()->user();
+        if ($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+        $this->clearLoginAttempts($request);
+
+        $token = (string) $request->user()->createToken($request->device)->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => null
+        ]);
+    }
+
+    protected function sendFailedLoginResponse(Request $request) 
+    {
+        //
+    }
+
+    public function validateLogin(Request $request)
+    {
+        return $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device' => 'required'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout']);
+});
+
+Route::group(['middleware' => 'guest:api'], function () {
+    Route::post('login', [\App\Http\Controllers\Auth\LoginController::class, 'login']);
 });
