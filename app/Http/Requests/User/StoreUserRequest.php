c981a2c410d6e20946e4c676046c09faf9269f10
Diego Castillo <diego.castilloguajardo@gmail.com>
2022-03-11 16:18:09 -0300

    init form request for user

---
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,'.Auth::id(),
+            'password' => 'required|string|min:8|max:255|confirmed',
+        ];
+    }
+}
